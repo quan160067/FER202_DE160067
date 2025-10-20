499dd22e7385d49bafbbbf8e44639f5183a38e26
Tran Dinh Quan <99698045+quan160067@users.noreply.github.com>
2025-10-20 16:53:07 +0700

    up

--- slot10/src/App.jsx
+++ slot10/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FooterPage from './pages/ FooterPage';
+import HomePage from './pages/HomePage';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <FooterPage />
+     <HomePage />
     </>
   )
 }
---
+++ slot10/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import HomeCarousel from "../component/Home/HomeCarousel";
+
+export default function HomePage() {
+  return (
+    <div>
+      <HomeCarousel />
+    </div>
+  );
+}
